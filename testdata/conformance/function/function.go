99e9105536887c6c85937be3904c8be7a0800037
Annie Fu <anniefu@google.com>
2022-08-23 11:33:43 -0700

    test: add buildpack integration test for nondeclarative functions

--- testdata/conformance/function/function.go
+++ testdata/conformance/function/function.go
@@ -16,17 +16,11 @@
 package function
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 
-	"cloud.google.com/go/functions/metadata"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/GoogleCloudPlatform/functions-framework-go/testdata/conformance/nondeclarative"
 )
 
 const (
@@ -35,61 +29,11 @@ const (
 
 // Register declarative functions
 func init() {
-	functions.HTTP("declarativeHTTP", HTTP)
+	functions.HTTP("declarativeHTTP", nondeclarative.HTTP)
 	functions.HTTP("concurrentHTTP", concurrentHTTP)
-	functions.CloudEvent("declarativeCloudEvent", CloudEvent)
+	functions.CloudEvent("declarativeCloudEvent", nondeclarative.CloudEvent)
 }
 
 func concurrentHTTP(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(1 * time.Second)
-}
-
-// HTTP is a simple HTTP function that writes the request body to the response body.
-func HTTP(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if err := ioutil.WriteFile(outputFile, body, 0644); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
-
-// Event is a background event function that dumps the data and context to JSON and calls the
-// validator script on the result.
-func Event(ctx context.Context, data interface{}) error {
-	m, err := metadata.FromContext(ctx)
-	if err != nil {
-		return fmt.Errorf("getting context metadata: %v", err)
-	}
-	event := struct {
-		Data    interface{}        `json:"data"`
-		Context *metadata.Metadata `json:"context"`
-	}{
-		Data:    data,
-		Context: m,
-	}
-	e, err := json.Marshal(event)
-	if err != nil {
-		return fmt.Errorf("marshalling event: %v", err)
-	}
-
-	log.Printf("writing %v, %s to file", event, string(e))
-	return ioutil.WriteFile(outputFile, e, 0644)
-}
-
-// CloudEvent is a cloud event function that dumps the event to JSON and calls the validator script
-func CloudEvent(ctx context.Context, ce cloudevents.Event) error {
-	e, err := json.Marshal(ce)
-	if err != nil {
-		return fmt.Errorf("marshalling cloud event: %v", err)
-	}
-
-	if err := ioutil.WriteFile(outputFile, e, 0644); err != nil {
-		return fmt.Errorf("writing file: %v", err)
-	}
-
-	return nil
 }
